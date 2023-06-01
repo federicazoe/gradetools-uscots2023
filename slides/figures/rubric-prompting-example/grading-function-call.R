e704dd9370475da3f89f984bc4c250bc533679ad
federicazoe <fzricci@uci.edu>
2023-06-01 00:58:19 -0400

    update slides

---
+++ slides/figures/rubric-prompting-example/grading-function-call.R
@@ -0,0 +1,9 @@
+gradetools::assist_grading(
+  rubric_path = here::here("slides", "figures", "rubric-prompting-example", "rubric.csv"), 
+  roster_path = here::here("slides", "figures", "rubric-prompting-example", "roster.csv"), 
+  grading_progress_log_path = here::here("slides", "figures", "rubric-prompting-example", "grading-log.csv"), 
+  final_grade_sheet_path = here::here("slides", "figures", "rubric-prompting-example", "grade-sheet.csv"), 
+  example_assignment_path = here::here("slides", "figures", "rubric-prompting-example", "student1-submission.R"),
+  example_feedback_path = here::here("slides", "figures", "rubric-prompting-example", "student1-feedback.html"), 
+  example_student_identifier = "student1"
+)
